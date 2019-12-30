771ddbb7bd2f965051680d25468f50d581466893
Biprodas Roy <biprodas.cse@gmail.com>
2019-12-30 13:39:01 +0600

    clean-client

---
+++ clean-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="text-center m-3">
+      <h2>Welcome to <span className="bg-warning font-weight-bold text-danger rounded px-3">Semi<span className="text-dark">colon</span></span></h2>
+    </div>
+  );
+}
+
+export default App;
